e469352f54c6d38976c951b0ea287f066bfcce10
Julian <jumpingafterrain@gmail.com>
2023-04-14 10:45:58 +0200

    not returning users yet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
+use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
 use dotenv;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-// mod database;
 // mod error;
 // mod handler;
 
@@ -20,10 +14,10 @@ async fn main() {
     // database
     let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
-        .max_connections(10)
+        .max_connections(5)
         .connect(&database_url)
         .await
-        .unwrap();
+        .expect("Failed to connect to the database");
 
     // initialize tracing
     tracing_subscriber::fmt::init();
@@ -31,7 +25,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(root))
+        .with_state(pool);
     // `POST /users` goes to `create_user`
     // .route("/users", post(create_user));
 
@@ -45,6 +40,34 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root(State(pool): State<PgPool>) -> Result<Json<Vec<String>>, (StatusCode, String)> {
+    sqlx::query_scalar("select * from public.\"User\";")
+        .fetch_all(&pool)
+        .await
+        .map(|users| {
+            for user in users.iter() {
+                println!("{:?}", user);
+            }
+            Json(users)
+        })
+        .map_err(internal_error)
+}
+
+struct User {
+    id: i32,
+    email: String,
+    password: String,
+    name: String,
+    phone: String,
+    is_pro: bool,
+    username: String,
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
